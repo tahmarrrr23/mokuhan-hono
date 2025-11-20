3bc10f85e0b3eca847d05a563516e7aebdc6a4da
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2025-11-20 22:31:19 +0900

    feat: add gracefully shut down

--- src/index.ts
+++ src/index.ts
@@ -28,6 +28,24 @@ app.doc("/openapi.json", {
   },
 });
 
-serve({ fetch: app.fetch, port: PORT, hostname: HOSTNAME }, (info) => {
-  console.log(`Server is running on http://${info.address}:${info.port}`);
+const server = serve(
+  { fetch: app.fetch, port: PORT, hostname: HOSTNAME },
+  (info) => {
+    console.log(`Server is running on http://${info.address}:${info.port}`);
+  },
+);
+
+process.on("SIGINT", () => {
+  server.close();
+  process.exit(0);
+});
+
+process.on("SIGTERM", () => {
+  server.close((err) => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    process.exit(0);
+  });
 });
