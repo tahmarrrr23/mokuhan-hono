053915d4dcebe398a0cc6e28c1dcee73b80fef51
Fumiya Yamashita <4547751+tahmarrrr23@users.noreply.github.com>
2026-08-22 23:53:24 +0900

    chore: update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,14 @@
+/// <reference types="node" />
+
 import "dotenv/config";
+
 import { defineConfig } from "drizzle-kit";
-
-const databaseUrl = process.env.DATABASE_URL;
-
-if (!databaseUrl) {
-  throw new Error("DATABASE_URL is not set");
-}
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema",
   dialect: "postgresql",
   dbCredentials: {
-    url: databaseUrl,
+    url: process.env.DATABASE_URL ?? "",
   },
 });
